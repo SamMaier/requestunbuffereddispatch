ba07157332cc2179f4f258a605b7165b7e117514
Sam Maier <smaier@google.com>
2020-06-10 11:57:43 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MyView.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        requestUnbufferedDispatch(event);
+        Log.e("application", "requested unbuffered dispatch");
+        return false;
+    }
+
+}
